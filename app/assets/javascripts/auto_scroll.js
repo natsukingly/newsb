5acbfdbbaa25e79d9485f845adf03da927eaa329
Natsuki <natsukingly01@gmail.com>
2018-02-01 20:40:48 +0000

    add no_notifications translation

--- app/assets/javascripts/auto_scroll.js
+++ app/assets/javascripts/auto_scroll.js
@@ -1,64 +1,17 @@
-
-
-
-
-
-
-
-
-
-
-// // infinite scroll
-// var win = $(window);
-// var loading = false;
-// var model = $('#main_contents').attr('class');
-
-// win.scroll(function() {
-// 	// for testing
-// 	// console.log("scrolltop" + win.scrollTop());
-// 	// console.log("document" + ($(document).height() - win.height()));
-	
-// 	// last number to trigger event before reaching the end of the document
-// 	if ($(document).height() - win.height() < win.scrollTop() + 1500) {
-// 		if (loading){
-// 			return;
-// 		}	
-// 		loading = true;		
-	
-// 		switch(model){
-// 			case "posts":
-// 				$.ajax({
-// 					url: "/posts/load_more",
-// 					type: "GET",
-// 					data: {existing_posts : $('#main_contents').find('.post').length },
-// 					dataType: 'script',
-// 				})
-// 				break;
-				
-// 			case "articles":
-//                 $.ajax({
-//                     url: "/categories/#{@category.id}/load_more",
-//                     type: "GET",
-//                     data: {existing_articles : $('#main_contents').find('.article').length },
-//                     dataType: 'script',
-//                 })
-// 				break;
-				
-// 			case "tags":
-// 				break;
-// 		}
-// 	}
-// });
-
-
-
-
-
-// if(#{ @not_found == true }){
-// 	$.ajax({
-// 		url: "/tags/articles_not_found",
-// 		type: "GET",
-// 		dataType: 'script',
-// 	})
-// 	// $('#main_contents').append("#{escape_javascript(render('shared/articles_not_found'))}");
-// }
+$(document).on('turbolinks:load', function() {
+	$('.scroll_btn.mobile_notifications').click(function(e){
+		e.stopPropagation();
+		var stop_loading = $(this).hasClass('stop_loading');
+		if(!(stop_loading)){
+			$.ajax({
+				url: "#{load_mobile_notifications_path}",
+				type: "GET",
+				data: {existing_notifications: $('.mobile_notifications').find('.notification').length,
+				},
+				dataType: 'script',
+			})
+			$(this).html("<i class='fa fa-spinner fa-pulse'></i>");
+			$(this).addClass("loading");
+		}
+	});
+});
